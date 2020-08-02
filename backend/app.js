a06ba24228ab3176c27ebe6d84859963509dc99f
aaizaen <max.concept89@gmail.com>
2020-08-02 18:02:33 +0200

    First commit - error to nodemon, files to check

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+//Création d'application express
+const app = express();
+
+//Import des routes
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+
+//Connection à la base de donnée MongoDB
+mongoose
+  .connect(
+    "mongodb+srv://test01:c02062012@coursp6.vrdlh.mongodb.net/CoursP6?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//Header pour contourner erreurs de CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+//Rendre la requete exploitable
+app.use(bodyParser.json());
+
+//Gestion de la ressource image de façon statique
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+//Routes attendues
+app.use("/api/auth", userRoutes);
+app.use("/api/sauces", sauceRoutes);
+
+module.exports = app;
